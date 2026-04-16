f2d9ccb216f3548560d65cc9b18cd7c6b050da5b
SHEROX-at <tobias.hoerandner@icloud.com>
2026-04-16 12:34:02 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,16 @@
 const db = {
-
-"Schienen":[
-{q:"Welche Schienenformen gibt es?",a:["71b,60E1,54E2,49E1","nur 60E1","keine","nur 71b"],c:0},
-{q:"Was beinhaltet das Walzzeichen?",a:["Walzwerk,Stahlsorte,Walzjahr,Form","nur Gewicht","nur Baujahr","keine"],c:0}
-],
-
-"Kleineisen":[
-{q:"Was sind Kleineisen?",a:["Befestigung der Schiene","nur Schrauben","nur Nägel","keine"],c:0}
-]
-
+"Oberbau": {
+  "Schienen": [
+    {q:"Schienenformen?",a:["71b,60E1","nur 60E1","keine","nur 71b"],c:0},
+    {q:"Walzzeichen?",a:["Werk, Stahl, Jahr","nur Gewicht","nur Jahr","keine"],c:0}
+  ],
+  "Kleineisen": [
+    {q:"Was sind Kleineisen?",a:["Befestigung","nur Schrauben","nur Nägel","keine"],c:0}
+  ]
+},
+"Weichen": {
+  "Grundlagen": [
+    {q:"Funktion von Weichen?",a:["Gleiswechsel","Stoppen","Bremsen","keine"],c:0}
+  ]
+}
 };
