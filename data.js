95001aea372a06e99cee6da4b79dcbb6aff4aa49
SHEROX-at <tobias.hoerandner@icloud.com>
2026-04-16 12:42:33 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,16 +1,15 @@
 const db = {
 "Oberbau": {
-  "Schienen": [
-    {q:"Schienenformen?",a:["71b,60E1","nur 60E1","keine","nur 71b"],c:0},
-    {q:"Walzzeichen?",a:["Werk, Stahl, Jahr","nur Gewicht","nur Jahr","keine"],c:0}
+  "Schienen":[
+    {q:"Schienenformen?",a:["71b,60E1","nur 60E1","keine","nur 71b"],c:0}
   ],
-  "Kleineisen": [
+  "Kleineisen":[
     {q:"Was sind Kleineisen?",a:["Befestigung","nur Schrauben","nur Nägel","keine"],c:0}
   ]
 },
 "Weichen": {
-  "Grundlagen": [
-    {q:"Funktion von Weichen?",a:["Gleiswechsel","Stoppen","Bremsen","keine"],c:0}
+  "Grundlagen":[
+    {q:"Funktion?",a:["Gleiswechsel","Stoppen","Bremsen","keine"],c:0}
   ]
 }
 };
