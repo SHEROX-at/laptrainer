8558fbbf226d9df88ffafc25445a5ba7fa37355b
SHEROX-at <tobias.hoerandner@icloud.com>
2026-04-16 12:54:02 +0200

    Update state.js

--- state.js
+++ state.js
@@ -1,4 +1,7 @@
-let currentMain=null;
-let currentSub=null;
-let quiz=[];
-let i=0;
+let currentMain = null;
+let currentSub = null;
+let quiz = [];
+let i = 0;
+
+/* 📊 STATISTIK */
+let stats = JSON.parse(localStorage.getItem("stats")) || {};
