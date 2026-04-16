d0b4942ba08b6a9a6368c66ef2c6e7459599fcbf
SHEROX-at <tobias.hoerandner@icloud.com>
2026-04-16 12:20:22 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const db = {
+
+"Schienen":[
+{q:"Welche Schienenformen gibt es?",a:["71b,60E1,54E2,49E1","nur 60E1","keine","nur 71b"],c:0},
+{q:"Was beinhaltet das Walzzeichen?",a:["Walzwerk,Stahlsorte,Walzjahr,Form","nur Gewicht","nur Baujahr","keine"],c:0}
+],
+
+"Kleineisen":[
+{q:"Was sind Kleineisen?",a:["Befestigung der Schiene","nur Schrauben","nur Nägel","keine"],c:0}
+]
+
+};
