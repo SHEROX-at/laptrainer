5736651dc0dbc7d58dfb62d95ea257ad0f7b08b1
SHEROX-at <tobias.hoerandner@icloud.com>
2026-04-16 13:38:13 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,18 @@
+const db = {
+"Oberbau": {
+  "Schienen":[
+    {q:"Schienenformen?",a:["71b,60E1","nur 60E1","keine","nur 71b"],c:0}
+  ],
+  "Kleineisen":[
+    {q:"Was sind Kleineisen?",a:["Befestigung","nur Schrauben","nur Nägel","keine"],c:0}
+  ]
+},
+"Weichen": {
+  "Grundlagen":[
+    {q:"Funktion?",a:["Gleiswechsel","Stoppen","Bremsen","keine"],c:0}
+  ]
+}
+};
 db["Unterbau"] = {
   "Eisenbahnkreuzungen": [
     {q:"Was ist eine Eisenbahnkreuzung?",a:["Kreuzung Straße-Bahn","nur Bahn","nur Straße","keine"],c:0}
