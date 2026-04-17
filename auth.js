2008c15652023fca2ee45c593de3bc805700d9bf
SHEROX-at <tobias.hoerandner@icloud.com>
2026-04-17 10:53:33 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,66 +1,33 @@
-const ACCESS_CODE = "1234";
+const auth = firebase.auth();
 
-/* REGISTER */
+// REGISTER
 function register() {
-  const user = document.getElementById("user").value.trim();
-  const pass = document.getElementById("pass").value.trim();
-  const code = document.getElementById("code").value.trim();
+  const email = username.value;
+  const password = password.value;
 
-  if (!user || !pass || !code) {
-    alert("Bitte alles ausfüllen");
-    return;
-  }
-
-  if (code !== ACCESS_CODE) {
-    alert("Falscher Zugangscode");
-    return;
-  }
-
-  let users = JSON.parse(localStorage.getItem("users") || "{}");
-
-  if (users[user]) {
-    alert("User existiert bereits");
-    return;
-  }
-
-  users[user] = {
-    password: pass,
-    progress: {}
-  };
-
-  localStorage.setItem("users", JSON.stringify(users));
-  localStorage.setItem("currentUser", user);
-
-  window.location.href = "app.html";
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(() => window.location = "app.html")
+    .catch(e => alert(e.message));
 }
 
-/* LOGIN */
+// LOGIN
 function login() {
-  const user = document.getElementById("user").value.trim();
-  const pass = document.getElementById("pass").value.trim();
+  const email = username.value;
+  const password = password.value;
 
-  let users = JSON.parse(localStorage.getItem("users") || "{}");
-
-  if (!users[user] || users[user].password !== pass) {
-    alert("Falsche Login Daten");
-    return;
-  }
-
-  localStorage.setItem("currentUser", user);
-
-  window.location.href = "app.html";
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => window.location = "app.html")
+    .catch(e => alert(e.message));
 }
 
-/* CHECK LOGIN */
-function checkAuth() {
-  const user = localStorage.getItem("currentUser");
-  if (!user) {
-    window.location.href = "index.html";
-  }
+// CHECK LOGIN
+function checkLogin() {
+  auth.onAuthStateChanged(user => {
+    if (!user) location.href = "index.html";
+  });
 }
 
-/* LOGOUT */
+// LOGOUT
 function logout() {
-  localStorage.removeItem("currentUser");
-  window.location.href = "index.html";
+  auth.signOut().then(() => location.href = "index.html");
 }
