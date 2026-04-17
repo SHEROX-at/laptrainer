716664b85ff5435fbc47798be1ae7b2872528122
SHEROX-at <tobias.hoerandner@icloud.com>
2026-04-17 16:41:38 +0200

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,12 @@
+const questions = [
+{
+q: "Was ist eine Weiche?",
+a: ["Gleiswechsel", "Auto", "Signal", "Brücke"],
+c: 0
+},
+{
+q: "Wozu dient Entwässerung?",
+a: ["Wasser ableiten", "Optik", "Strom", "Bremsen"],
+c: 0
+}
+];
